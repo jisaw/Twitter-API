67e8e214e8607b81ce6f52f3c0ce3457a60952c4
Jake Sawyer <jisawyer@iastate.edu>
2014-11-17 12:30:56 -0600

    Added limit to streaming twitter, added streaming xml file writer functionality

--- twit_stream.py
+++ twit_stream.py
@@ -5,22 +5,38 @@ import tweepy
 import logging as log
 import urllib2
 import json
+import xml.etree.cElementTree as ET
 
 OAUTH_TOKEN = '17796749-MPGh0m8blE7YJGcmZNG6iRgioyHOeQdzE0L8yaNIq'
 OAUTH_SECRET = 'Xo9WgvMMu5DgvH62GjBCF7pgSh8qD7HRUoJICY65R7Ts4'
 CONSUMER_KEY = 'Ny9DPm7eXF8R4Fk7djJdf8iUT'
 CONSUMER_SECRET = 'E1DlM4wAsqbWSkJdPMQ4gkuGfkA4ioOiGVfLDXh8pIZb2te5wr'
 
+param = ' '.join(sys.argv[1:])
+q = param[:param.find('?')]
+num = int(param[param.find('?')+1:])
+
 
 class StdOutListener(tweepy.StreamListener):
+    def __init__(self, api=None):
+        super(StdOutListener, self).__init__()
+        self.num_tweets = 0
+        self.write_list = []
+
     def on_data(self, data):
         # Twitter returns data in JSON format - we need to decode it first
         decoded = json.loads(data)
 
         # Also, we convert UTF-8 to ASCII ignoring all bad characters sent by users
         print '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
-        print ''
-        return True
+        self.write_list.append([decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore')])
+        self.num_tweets += 1
+        if self.num_tweets < int(num):
+            print ''
+            return True
+        else:
+            print(self.num_tweets)
+            return False
 
     def on_error(self, status):
         print status
@@ -35,7 +51,7 @@ def main():
     to_write = []
     param = ' '.join(sys.argv[1:])
     q = param[:param.find('?')]
-    #num = int(param[param.find('?')+1:])
+    num = int(param[param.find('?')+1:])
     print param
     print q
     #print num
@@ -50,12 +66,28 @@ def main():
         stream.filter(track=['%s' % query])
     except:
         log.warning('Cought Exception')
-    #write_xml(to_write)
+    filename = q.replace(" ", "_")
+    filename = filename.replace("(", "")
+    filename = filename.replace(")", "")
+    filename = filename.replace(":", "")
+    write_xml(l.write_list, filename)
 
-def write_xml(data):
+def write_xml(data, file_name):
     #TO-DO: write the data to an xml tree and save in a file
     print('Working on it')
+    thread = ET.Element('thread')
+    for tweet in data:
+        author = ET.SubElement(thread, 'author')
+        author.text = str(tweet[0])
 
+        text = ET.SubElement(thread, 'text')
+        text.text = str(tweet[1])
+
+    f = open("data/" + file_name + "streaming" + ".xml", 'a')
+    with f:
+        tree = ET.ElementTree(thread)
+        tree.write(f)
+    f.close()
 
 if __name__ == "__main__":
     main()
